6d7deac64353c337f9056519ab499f6227c406d3
vansh malik <vanshmalik18@gmail.com>
2021-04-14 17:36:33 +0530

    first text editor commit

---
+++ src/EditorSetup.js
@@ -0,0 +1,39 @@
+import { convertFromRaw, convertToRaw, EditorState } from 'draft-js';
+import draftToHtml from 'draftjs-to-html';
+import { React, useState } from 'react';
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
+
+
+const EditorSetup=()=>{
+    
+    const [editorState,setEditorState]=useState(()=>EditorState.createEmpty());
+
+    const [display, setDisplay]=useState('');
+    
+    const updateTextDesciption = async (state) =>{
+        await setEditorState(state);
+        setDisplay(draftToHtml(convertToRaw(editorState.getCurrentContent())))
+    }
+
+    return(
+        <div className="edit">
+        
+            <div className="editEditor">
+                <Editor
+                    editorState={editorState}
+                    toolbarClassName="toolbarClassName"
+                    wrapperClassName="wrapperClassName"
+                    editorClassName="editorClassName"
+                    onEditorStateChange={updateTextDesciption}
+                />
+            </div>
+            {/* <textarea
+                disabled
+                value={}
+            /> */}
+                <div dangerouslySetInnerHTML={{__html:`${display}`}}></div>
+        </div>
+    );
+}
+export default EditorSetup;
